abc9c4835b745d0dd1cc1c72b25f5737d62d3cc5
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-27 20:24:51 +0100

    fix: remove duplicate tags variable (already in context.tf)

--- variables.tf
+++ variables.tf
@@ -16,9 +16,3 @@ variable "routes" {
   type        = map(string)
   description = "Map of route key to Lambda function ARN (e.g. {\"$connect\" = \"arn:aws:lambda:...\"})"
 }
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Tags to apply to all resources"
-}
